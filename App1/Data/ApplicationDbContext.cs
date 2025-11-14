ab3e8ce52ea7e04b9a5e267ec1bdee758c8ae84b
Raghad-Moqady <raghadmoqady@gmail.com>
2025-11-14 17:21:39 +0200

    create new API project |Install packages|Create Models(Product & Category)| Create Data folder , migrations, update database

---
+++ App1/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using App1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace App1.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.;Database=API_app1;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+
+    }
+}
---
+++ App1/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace App1.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<Product> Products { get; set; } = new List<Product>();
+    }
+}
